b873a3b935e994a998078f362e2586a7f8a8e6d1
Vitaliy Antonov <agenorix@gmail.com>
2016-05-24 09:00:24 +0300

    Добавил либы от ZennoLab

--- sms-activate-lib/sms-activate-lib/sms-activate-lib/Class1.cs
+++ sms-activate-lib/sms-activate-lib/sms-activate-lib/Class1.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ZennoLab.CommandCenter;
+using ZennoLab.InterfacesLibrary;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+using ZennoLab.InterfacesLibrary.ProjectModel.Collections;
+using ZennoLab.InterfacesLibrary.ProjectModel.Enums;
+using ZennoLab.Macros;
+using Global.ZennoExtensions;
+using ZennoLab.Emulation;
 
 namespace sms_activate_lib
 {
