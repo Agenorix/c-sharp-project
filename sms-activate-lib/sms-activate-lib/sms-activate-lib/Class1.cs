ebb0ed46d5968a905bb3d086dfc2e3e48557eb93
Vitaliy Antonov <agenorix@gmail.com>
2016-05-24 12:46:26 +0300

    Добавил проверку баланса sms-activate.ru

--- sms-activate-lib/sms-activate-lib/sms-activate-lib/Class1.cs
+++ sms-activate-lib/sms-activate-lib/sms-activate-lib/Class1.cs
@@ -13,7 +13,17 @@ using ZennoLab.Emulation;
 
 namespace sms_activate_lib
 {
-    public class Class1
+    public class smsactivate
     {
+
+        private string ApiKey = string.Empty;
+        private string Proxy = string.Empty;
+        public string getbalance(string ApiKey, string Proxy)
+        {
+           string Balance = ZennoPoster.HttpGet("http://sms-activate.ru/stubs/handler_api.php?api_key=" + ApiKey + 
+               "&action=getBalance", Proxy, "UTF-8", ZennoLab.InterfacesLibrary.Enums.Http.ResponceType.BodyOnly);
+            return Balance;
+
+        }
     }
 }
