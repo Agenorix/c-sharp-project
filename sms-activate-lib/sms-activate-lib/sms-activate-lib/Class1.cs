a59c83a3a28cdba9791033831c16b7f17021d639
Vitaliy Antonov <agenorix@gmail.com>
2016-05-25 07:22:05 +0300

    Добавил паблик функцию getnumber и обработку ошибки sms-activate NO_KEY

--- sms-activate-lib/sms-activate-lib/sms-activate-lib/Class1.cs
+++ sms-activate-lib/sms-activate-lib/sms-activate-lib/Class1.cs
@@ -18,12 +18,25 @@ namespace sms_activate_lib
 
         private string ApiKey = string.Empty;
         private string Proxy = string.Empty;
-        public string getbalance(string ApiKey, string Proxy)
+
+        //Получаем баланс сервиса sms-activate.ru
+        private string getbalance(string ApiKey, string Proxy)
         {
            string Balance = ZennoPoster.HttpGet("http://sms-activate.ru/stubs/handler_api.php?api_key=" + ApiKey + 
                "&action=getBalance", Proxy, "UTF-8", ZennoLab.InterfacesLibrary.Enums.Http.ResponceType.BodyOnly);
             string balbal = System.Text.RegularExpressions.Regex.Replace(Balance, @".*?:", "");
             return balbal;
+        }
+
+        public string getnumber(string ApiKey, string Proxy)
+        {
+            //Получаем баланс сервиса sms-activate.ru
+            string asd = getbalance(ApiKey, Proxy);
+            if (asd.Contains("NO_KEY"))
+            {
+                return "Не указан ключ api сервиса sms-activate";
+            }
+            return asd;
 
         }
     }
