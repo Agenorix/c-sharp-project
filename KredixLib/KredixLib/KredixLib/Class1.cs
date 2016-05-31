7fb416ad207cc5103a08a41814a690494a42f10d
Vitaliy Antonov <agenorix@gmail.com>
2016-05-31 07:07:48 +0300

    Добавил и первоначально настроил проект KredixLib

---
+++ KredixLib/KredixLib/KredixLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZennoLab.CommandCenter;
+using ZennoLab.InterfacesLibrary;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+using ZennoLab.InterfacesLibrary.ProjectModel.Collections;
+using ZennoLab.InterfacesLibrary.ProjectModel.Enums;
+using ZennoLab.Macros;
+using Global.ZennoExtensions;
+using ZennoLab.Emulation;
+using System.Web;
+
+namespace KredixLib
+{
+    public class Class1
+    {
+    }
+}
