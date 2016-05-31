94bf4b617feab13e47f07de1accb2142c2267807
Vitaliy Antonov <agenorix@gmail.com>
2016-05-31 07:33:59 +0300

    Добавил необходимые переменные и проверку баланса sms-activate.ru

--- KredixLib/KredixLib/KredixLib/Class1.cs
+++ KredixLib/KredixLib/KredixLib/Class1.cs
@@ -14,7 +14,45 @@ using System.Web;
 
 namespace KredixLib
 {
-    public class Class1
+    public class kredixlib
     {
+        //Объявляем общие переменные
+        //Ключи API
+        private string ApiKey_smsreg = string.Empty;
+        private string ApiKey_smsactivate = string.Empty;
+        private string ApiKey_simsms = string.Empty;
+        private string ApiKey_smsvk = string.Empty;
+        private string ApiKey_smsarea = string.Empty;
+        private string ApiKey_onlinesim = string.Empty;
+
+        //Сервисы активации
+        private string smsactivate = string.Empty;
+        private string smsreg = string.Empty;
+        private string simsms = string.Empty;
+        private string smsvk = string.Empty;
+        private string smsarea = string.Empty;
+        private string onlinesim = string.Empty;
+
+        //Общие переменные
+        private string Proxy = string.Empty;
+        private string Service = string.Empty;
+        private string Forward = string.Empty;
+        private string Operator = string.Empty;
+        private string Number = string.Empty;
+        private string Id = string.Empty;
+
+        private string getbalanceall (string ApiKey_smsreg, string ApiKey_smsactivate, string ApiKey_simsms, string ApiKey_smsvk, string ApiKey_smsarea, string ApiKey_onlinesim)
+        {
+            //Получаем баланс sms-activate.ru
+            string smsactivate_getbalance = ZennoPoster.HttpGet("http://sms-activate.ru/stubs/handler_api.php?api_key=" + ApiKey_smsactivate +
+                "&action=getBalance", Proxy, "UTF-8", ZennoLab.InterfacesLibrary.Enums.Http.ResponceType.BodyOnly);
+
+            string smsactivate_balance = System.Text.RegularExpressions.Regex.Replace(smsactivate_getbalance, @".*?:", "");
+            if (smsactivate_balance == "0")
+            {
+                smsactivate = "null";
+            }
+            return "ok";
+        } 
     }
 }
